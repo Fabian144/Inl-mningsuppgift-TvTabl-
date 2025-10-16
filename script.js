f34bd987616e55277d6deb1860f99ac21481b813
Fabian144 <fabianyousif24@gmail.com>
2025-10-16 22:51:34 +0200

    Interval animation done

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
-//--------LÅT STÅ----------- 
-  // Definiera fasta "konstanter" för olika typer av animering av menyn
-  ANIMATION = { 
-    NONE: 'none',           // Ingen animation
-    TIMER: 'timer',         // setInterval-baserad animation
-    ALTERNATIVE: 'alternative' // ytterligare alternativ
-  };
+//--------LÅT STÅ-----------
+// Definiera fasta "konstanter" för olika typer av animering av menyn
+ANIMATION = {
+  NONE: "none", // Ingen animation
+  TIMER: "timer", // setInterval-baserad animation
+  ALTERNATIVE: "alternative", // ytterligare alternativ
+};
 
 // Ändra värdet för att styra vilken meny-animation som ska användas
-window.MENU_ANIMATION_MODE ??= ANIMATION.NONE; // ANIMATION.TIMER (Default) = ingen animation (G-nivå), ANIMATION.TIMER // (VG-nivå), ANIMATION.ALTERNATIVE // ytterligare ett alternativ (VG-nivå);
+window.MENU_ANIMATION_MODE ??= ANIMATION.TIMER; // ANIMATION.TIMER (Default) = ingen animation (G-nivå), ANIMATION.TIMER // (VG-nivå), ANIMATION.ALTERNATIVE // ytterligare ett alternativ (VG-nivå);
 
 /*
  Användningsexempel för animationer beroende på inställning
@@ -17,8 +16,22 @@ if (window.MENU_ANIMATION_MODE === ANIMATION.NONE) {
   console.log("Ingen meny-animation används");
 } else if (window.MENU_ANIMATION_MODE === ANIMATION.TIMER) {
   console.log("Meny-animation med timer används");
+
+  function toggleMenu() {
+    const menuPositionLeft = window.getComputedStyle(document.querySelector("ul.menu")).left;
+
+    if (parseFloat(menuPositionLeft) < 0) { // parseFloat returnerar alla float värden som är före en annan värdetyp ur en sträng, i det här fallet -300 ur "-300px"
+      let pos = parseFloat(menuPositionLeft);
+
+      const menuIntervalAnimation = setInterval(() => {
+        pos += 15;
+        document.querySelector("ul.menu").style.left = pos + "px";
+
+      if (pos > -1) clearInterval(menuIntervalAnimation);
+      }, 0);
+    }
+  }
 } else if (window.MENU_ANIMATION_MODE === ANIMATION.ALTERNATIVE) {
   console.log("Meny-animation med alternativ metod används");
 }
 //--------------------------
-
